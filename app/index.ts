6de7ab411578659ba51140b19363aad129d6d371
MiguelQueiroz <miguelqueiroz360@gmail.com>
2025-07-02 17:25:28 -0300

    Criação Banco De Dados No Host
    Criado o serviço de banco de dados postgresql e a sua comunicação com o BD

--- app/index.ts
+++ app/index.ts
@@ -1,19 +1,19 @@
 import express from 'express';
 import PG from "pg";
+import dotenv from 'dotenv';
 
+dotenv.config();
 var app = express();
-
-var client = new PG.Client();
+var client = new PG.Client(process.env.CONNECTION_STRING);
 
 app.get("/", async (req, res) => {
-    //const resp = await client.query("SELECT $1::text as message", ["Hello World"]);
-    //console.log(resp.rows[0].message);
+    const resp = await client.query("SELECT $1::text as message", ["Hello World"]);
 
-    res.send("resp.rows[0].message");
+    res.send(resp.rows[0].message);
 });
 
 app.listen(3300, async () => {
-    //await client.connect();
+    await client.connect();
 
     console.log("Iniciado ");
 });
