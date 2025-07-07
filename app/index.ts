56e2fbab21878daa7325d0d00aeb7a0285708a6d
MiguelQueiroz <miguelqueiroz360@gmail.com>
2025-07-06 21:43:47 -0300

    fix inversão listagem

--- app/index.ts
+++ app/index.ts
@@ -30,7 +30,7 @@ app.post("/criar", async (req, res) => {
 app.get("/listar/:offset/:width", async (req, res) => {
     try{
         const resp = await client.query("SELECT T.Id, T.Titulo, T.Descricao, S.StatusNome, T.DataDeCriacao " +
-            "FROM Tarefas AS T INNER JOIN StatusPossiveis AS S ON T.StatusTarefa = S.StatusId ORDER BY T.Id " +
+            "FROM Tarefas AS T INNER JOIN StatusPossiveis AS S ON T.StatusTarefa = S.StatusId ORDER BY T.Id DESC " +
             "LIMIT $1::int OFFSET $2::int;", [req.params.width, req.params.offset]
         );
 
